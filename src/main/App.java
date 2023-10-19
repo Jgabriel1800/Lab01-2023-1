b623f12343179fc15f65e7ccb0e4b469e4d56f1e
Jgabriel1800 <136657802+Jgabriel1800@users.noreply.github.com>
2023-10-19 20:20:42 +0000

    desafio1

--- src/main/App.java
+++ src/main/App.java
@@ -1,14 +1,30 @@
 package main;
+
 public class App {
     public static int encontre(int[] arrayOrdenado, int num){
-        return -1;
+        for( int i=0;i<arrayOrdenado.length;i++){
+            if(arrayOrdenado[i]==num){
+                return 1;
+            }
+        }
+            return 0;
+        }
+        public static int maior(int[] arrayDesordenado){            
+            int maior= arrayDesordenado[0];
+            
+            for (int i=0;i<arrayDesordenado.length;i++){
+                
+                if(arrayDesordenado[i]>= maior){
+                    
+                    maior = arrayDesordenado[i];
+                    
+                }
+            }
+            return maior;
+        }  
     }
 
-    public static int maior(int[] arrayDesordenado){
-        return -1;
-    }
 
-    public static Integer parMaisProximo(Integer[][] pares){
-        return 1;
-    }
-}
+        
+
+    
